f96ecc1771a32eedbdd09923d168cb354a7d169b
josyvine <64621103+josyvine@users.noreply.github.com>
2025-10-28 23:06:36 +0530

    Delete app/src/main/assets/vertex_shader.glsl

--- app/src/main/assets/vertex_shader.glsl
+++
@@ -1,9 +0,0 @@
-attribute vec4 vPosition;
-attribute vec2 vTexCoord;
-varying vec2 vTextureCoord;
-uniform mat4 uTransformMatrix;
-
-void main() {
-    gl_Position = vPosition;
-    vTextureCoord = (uTransformMatrix * vec4(vTexCoord, 0.0, 1.0)).xy;
-}
